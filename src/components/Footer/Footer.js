8be356ad972a86b69bb761abfa85b4a3528ccd4b
Andrew Nguyen <anguyen1320@gmail.com>
2020-05-04 10:32:51 -0500

    structure for landing page

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import { Link } from "react-router-dom";
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -8,6 +9,25 @@ import './Footer.css'
 
 const Footer = () => (
   <footer>
+    <div>
+      <div>
+        <h1>Endear</h1>
+      </div>
+      <div className="footer-nav-links">
+        <h3>Contact</h3>
+      </div>
+      <div className="footer-nav-links">
+        <Link to="/about">
+          <h3>About</h3>
+        </Link>
+      </div>
+      <div className="footer-nav-links">
+        <h3>Tips</h3>
+      </div>
+      <div className="footer-nav-links">
+        <h3>Follow</h3>
+      </div>
+    </div>
     &copy; Prime Digital Academy
   </footer>
 );
