cc0ff8b21f737b060b384f494e51ea3a05086610
Andrew Nguyen <anguyen1320@gmail.com>
2020-05-05 19:43:20 -0500

    setup method to handle city and zip code input values

--- src/components/RegistrationPages/LocationPage.js
+++ src/components/RegistrationPages/LocationPage.js
@@ -5,24 +5,45 @@ import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 
 class LocationPage extends Component {
+  state = {
+    city: "",
+    zipcode: "",
+  };
+
+  handleInputChangeFor = (propertyName) => (event) => {
+    this.setState({
+      [propertyName]: event.target.value,
+    });
+  };
+
   render() {
+    console.log(this.state);
+
     return (
       <div className="container">
         <Button>Back</Button>
         <h3>About You</h3>
         <h2>Where do you primarily live?</h2>
         <div className="input">
-          <input placeholder="City" type="text" name="city" />
-          <input
-            placeholder="Zip code"
-            type="text"
-            name="zipcode"
-            pattern="[0-9] {5}"
-            title="Five Digit zip code"
-          />
-        </div>
-        <div>
-          <Button>Next</Button>
+          <form onSubmit={this.registerLocation}>
+            <input
+              type="text"
+              name="city"
+              onChange={this.handleInputChangeFor("city")}
+            />
+            <label htmlFor="city">City</label>
+            <br></br>
+            <input
+              type="text"
+              name="zipcode"
+              pattern="[0-9] {5}"
+              title="Five Digit zip code"
+              onChange={this.handleInputChangeFor("zipcode")}
+            />
+            <label htmlFor="zipcode">Zip code</label>
+            <br></br>
+            <Button type="submit">Next</Button>
+          </form>
         </div>
       </div>
     );
