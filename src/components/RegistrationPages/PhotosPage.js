0674e510d20a9ea5ec716fc3e61e4301d5ec0cd4
Andrew Nguyen <anguyen1320@gmail.com>
2020-05-07 09:18:18 -0500

    setup serverside and client side s3

--- src/components/RegistrationPages/PhotosPage.js
+++ src/components/RegistrationPages/PhotosPage.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
 import { Button } from "@material-ui/core";
 import "./RegistrationPages.css";
+import DropzoneS3Uploader from "react-dropzone-s3-uploader";
 
 class PhotosPage extends Component {
-  handleBtn = (type) => (event) => {
-    if (type === "back") {
-      this.props.history.push("/interest");
-    }
-
-    if (type === "next") {
-      this.props.history.push("/details");
-    }
-  };
-
   render() {
+    const uploadOptions = {
+      server: "http://localhost:5000",
+      // signingUrlQueryParams: { uploadType: "avatar" },
+    };
+    const s3Url = "https://astrangethaobucket.s3.amazonaws.com";
     return (
       <div className="container">
-        <Button onClick={this.handleBtn("back")}>Back</Button>
+        <Button>Back</Button>
         <h3>About You</h3>
         <h2>Add some photos!</h2>
         <div className="input">
-          <input placeholder="images" />
+          <DropzoneS3Uploader
+            onFinish={this.handleFinishedUpload}
+            s3Url={s3Url}
+            maxSize={1024 * 1024 * 5}
+            upload={uploadOptions}
+          />
         </div>
         <div>
-          <Button onClick={this.handleBtn("next")}>Next</Button>
+          <Button>Next</Button>
         </div>
       </div>
     );
